a1cda80494434c7caaa6e71bd7eb544bfd61d3ae
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-11-26 11:56:23 +0100

    first commit

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "/"
+javascripts_dir = "/"
+fonts_dir = "/"
+output_style = :compressed
